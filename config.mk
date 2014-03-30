480e30ae05a0f8786b833fc3722247711811e042
Roberto <lasernav@gmail.com>
2014-03-30 16:56:45 +0200

    First Release

---
+++ config.mk
@@ -0,0 +1,26 @@
+
+# Select 'mega' for the 1280 APM1, 'mega2560' otherwise
+BOARD = mega2560
+
+# HAL_BOARD determines default HAL target.
+HAL_BOARD ?= HAL_BOARD_APM2
+
+# The communication port used to communicate with the APM.
+PORT = /dev/ttyACM0
+
+# uncomment and fill in the path to Arduino if installed in an exotic location
+# ARDUINO = /path/to/Arduino
+
+
+
+
+
+
+
+# VRBRAIN Firmware tree:
+VRBRAIN_ROOT=../VRBrain_nuttx
+
+# VRBRAIN NuttX tree:
+VRBRAIN_NUTTX_SRC=../VRBrain_nuttx/NuttX/nuttx
+
+
